641b7e1a5b91a65ff79ce2a7340457670f22f84b
TaniKroos <tanishsaini26@gmail.com>
2024-05-21 12:18:34 +0530

    <3

--- Router/src/components/Github/Github.jsx
+++ Router/src/components/Github/Github.jsx
@@ -2,15 +2,15 @@ import React, { useEffect, useState } from 'react'
 import { useLoaderData } from 'react-router-dom'
 function Github() {
     
-    const [data,setData] = useState([])
-    useEffect(() => {
-        fetch('https://api.github.com/users/TaniKroos')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            setData(data)
-        })
-    },[])
+    const data = useLoaderData()
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/TaniKroos')
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         console.log(data)
+    //         setData(data)
+    //     })
+    // },[])
     return (
         <div className='bg-yellow-100'>Github followers: {data.followers}
         <img src={data.avatar_url} className='rounded-lg ' alt="" /></div>
@@ -20,7 +20,7 @@ function Github() {
 export default Github
 
 
-// export const githubInfoLoader = async () => {
-//     const response = await fetch('https://api.github.com/users/TaniKroos')
-//     return response.json()
-// }
+export const githubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/TaniKroos')
+    return response.json()
+}
